52340a9246b01eb6c082ccfe309c4036c86cc548
Usman Umar <uteach38@gmail.com>
2025-09-19 08:46:05 -0700

    Friday Update-Mobile View Responsive

--- frontend/src/pages/auth/components/GoogleLogin.tsx
+++ frontend/src/pages/auth/components/GoogleLogin.tsx
@@ -1,83 +1,30 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { useGoogleLogin } from "@react-oauth/google";
-// import { useNavigate } from "react-router-dom";
-// import style from "../Auth.module.css";
-// import google from "../../../assets/icons/social/google.png";
-// import facebook from "../../../assets/icons/social/facebook.png";
+import { useSelector } from "react-redux";
+import style from "../Auth.module.css";
+import google from "../../../assets/icons/social/google.png";
+import facebook from "../../../assets/icons/social/facebook.png";
+import type { RootState } from "../../../store/store";
 
-// import { loginWithGoogle } from "../../../store/slices/authReducer";
-// import type { RootState } from "../../../store/store";
+const GoogleLogin = () => {
+  const { authMode } = useSelector((state: RootState) => state.auth);
+  
+  return (
+    <>
+      {(authMode === "login" || authMode === "register") && (
+        <div className={style.loginWith} id="flexCenter">
+          <button onClick={() => console.log("Google login clicked")}>
+            <img src={google} alt="google icon" className={style.Google} />
+          </button>
+          <button onClick={() => console.log("Facebook login clicked")}>
+            <img
+              src={facebook}
+              alt="facebook icon"
+              className={style.Facebook}
+            />
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
-// const GoogleLogin = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const usertype =
-//     useSelector((state: RootState) => state.auth.user?.usertype) || "patient";
-
-//   const login = useGoogleLogin({
-//     flow: "auth-code",
-//     onSuccess: (tokenResponse) => {
-//       // tokenResponse contains code or access_token
-//       console.log("Google Login Success:", tokenResponse);
-
-//       // Send this token/code to backend for verification
-//       dispatch(
-//         loginWithGoogle({
-//           credential: tokenResponse.code, // or tokenResponse.access_token
-//           usertype,
-//         })
-//       )
-//         .unwrap()
-//         .then((res: any) => {
-//           if (!res.exists) {
-//             navigate("/auth");
-//           } else {
-//             switch (res.user.usertype) {
-//               case "patient":
-//                 navigate("/patient/dashboard");
-//                 break;
-//               case "doctor":
-//                 navigate("/doctors/dashboard");
-//                 break;
-//               case "pharmacist":
-//                 navigate("/pharmacist/dashboard");
-//                 break;
-//               case "admin":
-//                 navigate("/admin/dashboard");
-//                 break;
-//               default:
-//                 navigate("/");
-//             }
-//           }
-//         })
-//         .catch((err: any) => {
-//           console.error("Google login error", err);
-//         });
-//     },
-//     onError: () => {
-//       console.log("Google login failed");
-//     },
-//   });
-
-//   const { authMode } = useSelector((state: RootState) => state.auth);
-//   return (
-//     <>
-//       {(authMode === "login" || authMode === "register") && (
-//         <div className={style.loginWith} id="flexCenter">
-//           <button onClick={() => login()}>
-//             <img src={google} alt="google icon" className={style.Google} />
-//           </button>
-//           <div>
-//             <img
-//               src={facebook}
-//               alt="facebook icon"
-//               className={style.Facebook}
-//             />
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default GoogleLogin;
+export default GoogleLogin;
